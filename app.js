708012d7f38d2a5a69f8700147bc831a97e9753d
MalboreX <playmalborex@gmail.com>
2020-10-04 13:38:25 -0400

    some test

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const express = require('express')
+const cors = require('cors')
 
 const app = express()
 
+
 app.listen(5000, () => console.log('App has been started...'))
 
-app.use('/', function(request, response) {
-    response.end('<h1>I\'m just express</h1>')
+app.use(express.json());
+app.use(cors());
+
+app.get('/api/v1/schedule', function(request, response) {
+    const timetable = {
+        items: {
+            'm': '1'
+        }
+    }
+
+    return response.json(timetable)
 })
--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -2,18 +2,54 @@ import React from 'react';
 
 import Timetable from './Timetable'
 
-function Main() {
-    return (
-        <main>
-            <div className = "row schedule-container">
-                <Timetable partyName = "П-17"/>
-                <Timetable partyName = "П-18"/>
-                <Timetable partyName = "П-19"/>
-                <Timetable partyName = "П-20"/>
-                <Timetable partyName = "П-21"/>
-            </div>
-        </main>
-    )
+class Main extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            items: [],
+            isLoaded: false
+        }
+    }
+
+    componentDidMount() {
+        fetch('http://localhost:5000/api/v1/schedule')
+        .then(res => res.json())
+        .then(
+            (result) => {
+                this.setState({
+                    isLoaded: true,
+                    items: result.items
+                });
+            },
+            (error) => {
+                this.setState({
+                    isLoaded: false,
+                    error
+                });
+            }
+        )
+    }
+
+    render() {
+        const { error, isLoaded, items} = this.state;
+        
+        if (isLoaded) {
+            console.log(items);
+        }
+
+        return (
+            <main>
+                <div className = "row schedule-container">
+                    <Timetable partyName = "П-17"/>
+                    <Timetable partyName = "П-18"/>
+                    <Timetable partyName = "П-19"/>
+                    <Timetable partyName = "П-20"/>
+                    <Timetable partyName = "П-21"/>
+                </div>
+            </main>
+        )
+    }
 }
 
 export default Main
