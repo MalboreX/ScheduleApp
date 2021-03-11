4fb98a1dcb76c261c4226b3d3e0698935a9092d0
malborex <playmalborex@gmail.com>
2021-03-11 14:04:58 +0300

    added TeacherController.post()

--- server/src/api/controllers/teacherController.js
+++ server/src/api/controllers/teacherController.js
@@ -5,16 +5,40 @@ exports.getTeachers = async (req, res, next) => {
   try {
     await Teacher.find({}, (err, data) => {
       if(err) {
-        return next(
-          new AppError(500, 'fail', 'Sometimes shit happens'),
-          req,
-          res,
-          next
-        )
+        return next(new AppError(500, 'fail', 'Sometimes shit happens'), req, res, next)
       } else {
           return res.status(200).json(data)
       }
     })
+  }
+  catch(err) {
+    next(err)
+  }
+}
+
+exports.addTeachers = async (req, res, next) => {
+  try {
+    const name = req.body.name
+    const spec = req.body.spec
+
+    if(name && spec) {
+      const teacher = new Teacher({
+        'name': name,
+        'spec': spec
+      })
+
+      await Teacher.save(teacher)
+      .then(result => {
+        return res.status(200).json({
+          result: 'success'
+        })
+      })
+      .catch(err => {
+        return next(new AppError(500, 'fail', 'Sometimes shit happens'), req, res, next)
+      })
+    } else {
+      return next(new AppError(449, 'fail', 'Please provide name and spec'), req, res, next)
+    }
   }
   catch(err) {
     next(err)
--- server/src/api/routes/teacherRoutes.js
+++ server/src/api/routes/teacherRoutes.js
@@ -4,5 +4,6 @@ const router = express.Router()
 const teacherController = require('../controllers/teacherController')
 
 router.get('/', teacherController.getTeachers)
+router.post('/', teacherController.addTeachers)
 
 module.exports = router
